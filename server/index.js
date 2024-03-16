6f6097e75a2dee1d8e4f362e5b36063e3c93bcb7
Aakash <akjadhav2212@gmail.com>
2024-03-16 09:11:14 +0530

    added the .env file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const { Server }  = require("socket.io");
 const {createServer} = require('http')
-
-
+require('dotenv').config();
 const httpServer = createServer();
 const uuidToData = new Map();
 const io = new Server(httpServer, {
@@ -23,4 +22,6 @@ io.on('connection', socket => {
     })
 
 });
-httpServer.listen(3000,()=>{'server is listening on port 3000'});
+const port = process.env.PORT || 3000;
+console.log(port);
+httpServer.listen(port,()=>{'server is listening on port',port});
